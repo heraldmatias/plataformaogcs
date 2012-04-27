303ccfcbb9cb60f4cc2c8db6990ab3640c8441e9
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2012-04-27 15:45:47 -0500

    dia2

---
+++ src/apps/scripts/scripts.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+def imprimirToExcel(template,parametros,filename):
+    html = render_to_string(template, parametros,)
+    reporte = HttpResponse(content_type='application/ms-excel')
+    reporte['Content-Disposition'] = 'attachment; filename='+filename
+    reporte.write(html)    
+    return reporte
