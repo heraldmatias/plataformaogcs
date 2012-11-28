c20322feee3b2eaa30cc33d0c0d171c6743fcf70
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2012-11-28 15:08:45 -0500

    multi view calendar

--- src/apps/calendario/calendar.py
+++ src/apps/calendario/calendar.py
@@ -159,7 +159,7 @@ def _InsertEvent(cal_client, title='Tennis with Beth',
 def getinframe(calendar_name = None):
     feed = calendar_client.GetOwnCalendarsFeed()
     begin="""<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;ctz=America%2FLima&amp;
-    showPrint=0&amp;showTabs=0&amp;showTz=0&amp;height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;"""
+    showPrint=0&amp;showTz=0&amp;height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;"""
     end='style=" border-width:0 " width="690" height="400" frameborder="0" scrolling="no"></iframe>';
     result=''
     result+=begin
